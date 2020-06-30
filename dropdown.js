0a1d8db0ba25946bc11d511a1af6c4df104122d7
lunabrSP <67156644+lunabrSP@users.noreply.github.com>
2020-06-30 19:41:20 -0300

    Versão Melhor

--- dropdown.js
+++ dropdown.js
@@ -1,46 +1,19 @@
-var templateUSR = `<div class="row rowuser">
-                        <div class="col-md-6 comborda">
-                            <h3> **NOME** </h3>
-                            Racf: **RACF** <br>
-                            Email: **EMAIL** <br>
-                        </div>
-                        
-                        <div class="col-mf-4 comborda">
-                            <h4>**NOMEDEPTO**</h4>
-                            Unidade: **UNIDADE** <br>
-                            Andar: **ANDAR**
-                        </div>
-                    </div>`;
-
-
-var templategente = `<div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Parceiro
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-<a class="dropdown-item" href="parceiro1.html">Parceiro 1</a>
-<a class="dropdown-item" href="parceiro2.html">Parceiro 2</a>
-<a class="dropdown-item" href="parceiro3.html">Parceiro 3</a>
-</div>
-`
+var tptDDLBegin = `<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">`
+var tptDDLEnd = `</div>`
+var templateAgente = `<a class="dropdown-item" href="dashboard.html?id=**IDAGENTE**">**NOMEAGENTE**</a>`
 
 function carregaParceiros(){
-
     fetch("http://localhost:8080/nomeagentes")
         .then(res => res.json())
-        .then(res => preenche(res));
-}
+        .then(res => preenche(res)); }
 
 function preenche(resJson){
-    console.log(resJson);
-    var contSTR ="";
-    for(i=0; i<resJson.length; i++){
-        var user = resJson[i];
-
-        var novaLinha = templateUSR.replace("**NOME**",user.id_agente)
-                                    .replace("**LINKFOTO**",user.nome_agente);
-                                            
-        contSTR = contSTR + novaLinha;
+    var contAgente ="";
+    for(i=0; i<resJson.length; i++){       
+        var agente = resJson[i];
+        contAgente = contAgente + templateAgente.replace("**IDAGENTE**",agente.id_agente).replace("**NOMEAGENTE**", agente.nome_agente);
+        console.log("debug " + contAgente);
     }
-    document.getElementById("conteudo").innerHTML = contSTR;
-
+    console.log("debug 2" + tptDDLBegin + contAgente + tptDDLEnd);
+    document.getElementById("conteudo").innerHTML = tptDDLBegin + contAgente + tptDDLEnd;
 }
